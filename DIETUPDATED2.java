f6b7fee9fd4b46d59d42be5d708036b2e10a7cc7
MVRK0 <elikemfenuksu@gmail.com>
2024-06-01 19:19:38 +0000

    refactor: using a single method to cater to all cases

--- DIETUPDATED2.java
+++ DIETUPDATED2.java
@@ -14,68 +14,41 @@ public class DIETUPDATED2 {
         //initializing the time object to store the time for midnight and noon to the variables midnight and noon
         int hour = time.getHour();
 
-        switch(hour) {
+        switch (hour) {
             //morning
             case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 11:
-            System.out.println("It's morning. How many breakfast menu items do you have in mind?: ");
-            try {
-                //storing the number of breakfast items the user wants; this will be used as the length of the string array storing the breakfast menu items
-                int breakfastmenu = input.nextInt();
-                //consuming the newline character to prevent problems when input.nextLine() is called
-                input.nextLine();
-                //initializing a string array to store the breakfast menu items; the length is determined by the number in the variable breakfast menu
-                String[] breakfast = new String[breakfastmenu];
-                //for loop to iterate through the indexes in the array, storing the breakfast menu items
-                for (int i = 0; i < breakfastmenu; i++) {
-                    System.out.println("Enter the breakfast menu item: ");
-                    breakfast[i] = input.nextLine();
-                }
-                //initializing the random object to return a random integer bound to the integer value in the variable breakfast menu; the random integer will be used to reference a random index in the array
-                int randombreakfast = random.nextInt(breakfastmenu);
-                //printing the breakfast item with the randomly generated index
-                System.out.println("You will be having " + breakfast[randombreakfast] + " for breakfast.");
+                randomizer(input, random, "breakfast");
                 break;
-            }
-            catch (Exception e) {
-                System.out.println("enter digits only");
-            }
             //afternoon
             case 12: case 13: case 14: case 15: case 16: case 17: case 18:
-            System.out.println("it's noon. How many lunch menu items do you have in mind?: ");
-            try {
-                int lunchmenu = input.nextInt();
-                input.nextLine();
-                String[] lunch = new String[lunchmenu];
-                for (int m = 0; m < lunchmenu; m++) {
-                    System.out.println("Enter the lunch menu item: ");
-                    lunch[m] = input.nextLine();
-                }
-                int randomlunch = random.nextInt(lunchmenu);
-                System.out.println("You will be having " + lunch[randomlunch] + " for lunch.");
-            }
-            catch (Exception e) {
-                System.out.println("enter digits only");
-            }
+                randomizer(input, random, "lunch");
+                break;
             //evening
             case 19: case 20: case 21: case 22: case 23:
-            System.out.println("it's evening. How many supper menu items do you have in mind?: ");
-            try {
-                int suppermenu = input.nextInt();
-                input.nextLine();
-                String[] supper = new String[suppermenu];
-                for (int n = 0; n < suppermenu; n++) {
-                    System.out.println("Enter the supper menu item");
-                    supper[n] = input.nextLine();
-                }
-                int randomsupper = random.nextInt(suppermenu);
-                System.out.println("You will be having " + supper[randomsupper] + " for supper.");
+                randomizer(input, random, "supper");
                 break;
-            }
-            catch (Exception e) {
-                System.out.println("enter digits only");
-            }
             default:
-                System.out.println("invalid input");
+                System.out.println("Invalid input. Try again.");
+        }
+    }
+
+    //method to handle the menu updating and output of random foods
+    public static void randomizer (Scanner input, Random random, String mealtype){
+        try {  //handling the event that a user might enter letters instead of digits
+            System.out.println("It's " + mealtype + " time. How many " +  mealtype + "menu items do you have in mind?: ");
+            int nummenu = input.nextInt();
+            input.nextLine();                               //consuming the newline character
+
+            String[] Menu = new String[nummenu];            //initializing an empty string array
+            for (int i = 0; i < Menu.length; i++) {         // updating the array with the menu items
+                System.out.println("Enter the menu item");
+                Menu[i] = input.nextLine();                 //storing the menu items to the available indexes
+            }
+            int randomfood = random.nextInt(nummenu);       //initializng a random object to generate random numbers bound to the nummber of menu items
+            System.out.println("You will be having " + Menu[randomfood] + ".");
+
+        } catch (Exception e) {
+            System.out.println("enter digits only");
         }
     }
 }
